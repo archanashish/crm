e9b11a63a7a2db6428664d0d35af5127595a0813
Ashish Thete <archanashinde68@gmail.com>
2018-11-22 00:05:56 +0530

    First commit

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('Ashish Thete');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'incorrect'
+        ));
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
